dd0af0a0c8764a7468026b698c03211d1bc54ffb
Fred F <f--f@users.noreply.github.com>
2019-06-14 15:36:37 -0400

    Add txt/mcd parsers

---
+++ reader.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import xarray as xr
+import tifffile as tiff
+
+from pathlib import Path
+from typing import Union, List, Sequence
+import mmap
+import xmltodict
+import struct
+import re
+
+
+def parse_channel_headers(headers: Sequence[str]) -> List[str]:
+    """Extract channel and label from text column headers."""
+    label, metal, mass = zip(*[re.findall("(.+)\(([a-zA-Z]+)(\d+)Di\)", c)[0] for c in headers])
+    channel = [f"{met}({m})" for met, m in zip(metal, mass)]
+    return [f"{ch}_{l}" for ch, l in zip(channel, label)]
+
+
+def xyzc_to_arr(arr: np.ndarray) -> np.ndarray:
+    """Transform long-form data with (x, y, z, *channels) columns to an image array
+    indexed by (row, col, channel)."""
+    xsz, ysz = arr[:,:2].max(axis=0).astype(int) + 1
+    csz = arr.shape[1] - 3
+    return arr[:,3:].reshape((ysz, xsz, csz))
+
+
+def read_txt(path: Union[Path, str]) -> xr.DataArray:
+    """Read a Fluidigm IMC .txt file and return as a xarray DataArray."""
+    txt = pd.read_csv(path, sep="\t")
+    # Validate text file columns
+    expected_cols = ("Start_push", "End_push", "Pushes_duration", "X", "Y", "Z")
+    if tuple(txt.columns[:6]) != expected_cols:
+        raise ValueError(
+            f"'{str(path)}' is not valid IMC text data (expected first 6 columns: {expected_cols})."
+        )
+    txt = txt.drop(columns=list(expected_cols[:3]))  # Drop first three columns
+    channel_names = parse_channel_headers(txt.columns[3:])
+    img = xyzc_to_arr(txt.values).astype(np.float32)
+    return xr.DataArray(img, name=Path(path).stem, dims=("y", "x", "c"),
+        coords={"x": range(img.shape[1]), "y": range(img.shape[0]), 
+                "c": channel_names})
+
+
+def read_mcd(path: Union[Path, str], encoding: str="utf-16-le") -> List[xr.DataArray]:
+    """Read a Fluidigm IMC .mcd file and return as a list of xarray DataArray
+    (since MCD files can contain more than one image). 'encoding' specifies the Unicode
+    encoding of the XML section (defaults to UTF-16-LE)."""
+    imgs = []
+    with open(path, mode="rb") as fh:
+        mm = mmap.mmap(fh.fileno(), 0, access=mmap.ACCESS_READ)
+        # MCD format documentation recommends searching from end for "<MCDPublic"
+        offset = mm.rfind("<MCDPublic".encode(encoding))
+        if offset == -1:
+            raise ValueError(f"'{str(path)}' does not contain MCDPublic XML footer (try different encoding?).")
+        mm.seek(offset)
+        xml = mm.read().decode(encoding)
+        # Parse xml, force Acquisition(Channel) to be list even if one item so we can iterate
+        # (Can have multiple acquisitions/images in the same file...)
+        root = xmltodict.parse(xml, force_list=("Acquisition", "AcquisitionChannel"))["MCDPublic"]
+        acquisitions = root["Acquisition"]
+        for acq in acquisitions:
+            # Get acquisition ID
+            id_ = acq["ID"]
+            # Get list of channels for this acquisition
+            channels = [ch for ch in root["AcquisitionChannel"] 
+                if ch["AcquisitionID"] == id_ and ch["ChannelName"] not in ("X", "Y", "Z")]
+            channels = sorted(channels, key=lambda c: int(c["OrderNumber"]))
+            # Parse 4-byte float32 values
+            # Data consists of point values ordered X, Y, Z, C1, C2, ..., CN (and so on)
+            if acq["SegmentDataFormat"] != "Float" or acq["ValueBytes"] != "4":
+                raise NotImplementedError("Expected float32 data in 'SegmentDataFormat' tag.")
+            mm.seek(int(acq["DataStartOffset"]))
+            raw = mm.read(int(acq["DataEndOffset"]) - int(acq["DataStartOffset"]))
+            arr = np.array(
+                [struct.unpack("f", raw[i:i+4])[0] for i in range(0, len(raw), 4)],
+                dtype=np.float32
+            ).reshape((-1, len(channels) + 3))
+            img = xyzc_to_arr(arr)
+            channel_names = [
+                f"{c['ChannelName']}_{c['ChannelLabel']}" 
+                if c["ChannelLabel"] is not None else c["ChannelName"] for c in channels
+            ]
+            imgs.append(
+                xr.DataArray(img, name=f"{Path(path).stem}_{id_}",
+                    dims=("y", "x", "c"), 
+                    coords={"x": range(img.shape[1]), "y": range(img.shape[0]),
+                            "c": channel_names},
+                    attrs=acq,
+                )
+            )
+        mm.close()
+    return imgs
