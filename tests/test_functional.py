d857929f28a6ae0fe8a97376006cbab38d8c4c11
Fred F <f--f@users.noreply.github.com>
2019-12-06 12:35:09 -0500

    Test for shuffled data rows

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,7 +5,7 @@ import pickle
 from pathlib import Path
 
 import pytest
-from imcconv import read_txt
+from imcconv import read_txt, ROIData
 
 
 def unpickle_ref(path):
@@ -39,3 +39,11 @@ def test_read_txt_fill_missing_xy_rows(txt_missing_xy_rows_path):
     arr = read_txt(txt_missing_xy_rows_path, fill_missing=-1)
     refarr = unpickle_ref(txt_missing_xy_rows_path)
     assert arr.equals(refarr)
+
+
+def test_read_txt_on_shuffled_rows(txt_valid_path):
+    roi = ROIData.from_txt(txt_valid_path)
+    arr = roi.as_dataarray(None)
+    roi.df = roi.df.sample(frac=1)
+    arr_shuffled = roi.as_dataarray(None)
+    assert arr.equals(arr_shuffled)
